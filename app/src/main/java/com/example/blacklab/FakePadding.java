4536521a9d4fbd3ec94ca93fee88e698af970e80
Hardik Rajpal <hardikrajpal@microsoft.com>
2025-06-11 14:31:29 +0530

    left and right boys

--- app/src/main/java/com/example/blacklab/FakePadding.java
+++ app/src/main/java/com/example/blacklab/FakePadding.java
@@ -13,16 +13,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FakePadding extends AppCompatActivity implements FakeStripHolder{
+public class FakePadding extends AppCompatActivity {
 
     private Insets mCachedInsets;
     private FakeStripView mFakeStrip;
-
-    @NonNull
-    @Override
-    public void setFakeStrip(FakeStripView view) {
-        mFakeStrip = view;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +30,8 @@ public class FakePadding extends AppCompatActivity implements FakeStripHolder{
             public WindowInsets onApplyWindowInsets(@NonNull View v, @NonNull WindowInsets insets) {
                 Insets insets1 = insets.getInsets(WindowInsets.Type.systemBars() | WindowInsets.Type.displayCutout());
                 mCachedInsets = insets1;
-                FakePaddingHelper.createFakeStripView(getRootview(), getFirstContainer(), insets1, FakePadding.this);
+                FakePaddingHelper.applyFakePadding(getRootview(), getFirstContainer(), insets1);
                 return WindowInsets.CONSUMED;
-            }
-        });
-        getRootview().getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                if (mCachedInsets != null && mFakeStrip != null) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                        int[] colors = FakePaddingHelper.getColors(getFirstContainer());
-                        mFakeStrip.updateColors(colors);
-                    }
-                }
-                return true;
             }
         });
     }
--- app/src/main/java/com/example/blacklab/FakePaddingHelper.kt
+++ app/src/main/java/com/example/blacklab/FakePaddingHelper.kt
@@ -3,6 +3,7 @@ package com.example.blacklab
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Insets
+import android.graphics.Paint
 import android.os.Build
 import android.view.View
 import android.view.ViewGroup
@@ -13,58 +14,139 @@ import androidx.core.view.children
 object FakePaddingHelper {
     @RequiresApi(Build.VERSION_CODES.Q)
     @JvmStatic
-    fun createFakeStripView(rootView: ViewGroup, viewToExtend: View, insets1: Insets, holder: FakeStripHolder) {
-        val left = insets1.left
+    fun applyFakePadding(rootView: ViewGroup, viewToExtend:View, padding: android.graphics.Insets) {
+        val left = padding.left
+        val right = padding.right;
         // real padding
-        rootView.setPadding(0, insets1.top, insets1.right, insets1.bottom)
-        // fake padding
-        // TODO left and right.
-        if (left == 0) {
-            return
-        }
-        rootView.post {
-            val fakeStripView = getFakeStripView(viewToExtend, left)
-            val params = ViewGroup.LayoutParams(left, viewToExtend.height)
-            for (child in rootView.children.toList()) {
+        rootView.setPadding(0, padding.top, 0, padding.bottom)
+        if (left + right == 0) {
+            for(child in rootView.children.toList()){
                 if (child is FakeStripView) {
                     rootView.removeView(child)
                 }
             }
-            holder.setFakeStrip(fakeStripView)
-            rootView.addView(
-                fakeStripView,
-                0,
-                params
-            )
+            return
+        }
+        // fake padding
+        rootView.post {
+            var fakeStripExists = false
+            for(child in rootView.children.toList()){
+                if (child is FakeStripView) {
+                    fakeStripExists = true
+                    child.updateColorsAndWidth(padding)
+                }
+            }
+            if (!fakeStripExists) {
+                val paramsLeft = ViewGroup.LayoutParams(left, viewToExtend.height)
+                val paramsRight = ViewGroup.LayoutParams(right, viewToExtend.height)
+                val fakeStripViewLeft = getFakeStripView(viewToExtend, left, isLeft = true)
+                val fakeStripViewRight = getFakeStripView(viewToExtend, right, isLeft = false)
+                rootView.addView(
+                    fakeStripViewLeft,
+                    0,
+                    paramsLeft
+                )
+                rootView.addView(fakeStripViewRight, -1, paramsRight)
+                rootView.viewTreeObserver.addOnPreDrawListener {
+                    for(child in rootView.children.toList()){
+                        if (child is FakeStripView) {
+                            child.updateColorsAndWidth(null)
+                        }
+                    }
+                    true
+                }
+            }
+
         }
     }
 
-    private fun getFakeStripView(viewToExtend: View, @Px stripWidth: Int): FakeStripView {
-        val colors = getColors(viewToExtend)
-        return FakeStripView(viewToExtend.context, colors, stripWidth)
+    private fun getFakeStripView(viewToExtend: View, @Px stripWidth: Int, isLeft: Boolean): View {
+        val colors = getColorArray(viewToExtend, isLeft)
+        return FakeStripView(viewToExtend, colors, stripWidth, isLeft)
     }
-    @JvmStatic
-    fun getColors(viewToExtend: View): IntArray {
+
+    fun getColorArray(
+        viewToExtend: View,
+        isLeft: Boolean
+    ): IntArray {
         val colorStripsView: View = viewToExtend
+        val observedPixelsWidth = 1
+        val viewHeight = colorStripsView.height
         val bitmap = Bitmap.createBitmap(
-            colorStripsView.width,
-            colorStripsView.height,
+            /*width*/observedPixelsWidth,
+            viewHeight,
             Bitmap.Config.ARGB_8888
         )
         val canvas = Canvas(bitmap)
         colorStripsView.draw(canvas)
-        val colors = IntArray(colorStripsView.height)
-        for (y in 0 until colorStripsView.height) {
-            colors[y] = bitmap.getPixel(0, y)
+        if (!isLeft) {
+            // for the right strip, translate the canvas
+            // so the rightmost pixels come into the bitmap
+            val dx = -(colorStripsView.width - observedPixelsWidth).toFloat()
+            val dy = 0f
+            canvas.translate(dx, dy)
+            // clip so only the line of rightmost pixels is drawn
+            canvas.clipRect(0, 0, observedPixelsWidth, viewHeight)
         }
+        val colors = IntArray(viewHeight)
+        val startX = 0
+        for (y in 0 until viewHeight) {
+            colors[y] = bitmap.getPixel(startX, y)
+        }
+        bitmap.recycle()
         return colors
     }
-
-    @JvmStatic
-    fun updateFakeStripView(mFakeStrip: FakeStripView, FirstContainer: View) {
-        mFakeStrip
-    }
 }
-interface FakeStripHolder {
-    fun setFakeStrip(view: FakeStripView)
+
+class FakeStripView(val viewToExtend: View, var colors: IntArray, var stripWidth: Int, val isLeft: Boolean) : View(viewToExtend.context) {
+    fun updateColorsAndWidth(padding: android.graphics.Insets?){
+        var mustInvalidate = false
+        if (padding != null) {
+            val newWidth = if (isLeft) padding.left else padding.right
+            if (newWidth != stripWidth) {
+                stripWidth = newWidth
+                mustInvalidate = true
+            }
+            else {
+                if (stripWidth == 0) {
+                    return
+                }
+            }
+        }
+        val newColors = FakePaddingHelper.getColorArray(viewToExtend, isLeft)
+        if (!newColors.contentEquals(colors)) {
+            colors = newColors
+            mustInvalidate = true
+        }
+        if (mustInvalidate) {
+            invalidate()
+        }
+    }
+    override fun onDraw(canvas: Canvas) {
+        var startY = 0
+        var currentColor = colors[0]
+        val paint = Paint()
+        for (y in colors.indices) {
+            if (colors[y] != currentColor) {
+                paint.color = currentColor
+                canvas.drawRect(
+                    0f,
+                    startY.toFloat(),
+                    stripWidth.toFloat(),
+                    y.toFloat(),
+                    paint
+                )
+                startY = y
+                currentColor = colors[y]
+            }
+        }
+        paint.color = currentColor
+        canvas.drawRect(
+            0f,
+            startY.toFloat(),
+            stripWidth.toFloat(),
+            colors.size.toFloat(),
+            paint
+        )
+    }
 }
